a52ce0e3e104a99e252582fc5e483d55d4fd7380
Natuski <1050100964@qq.com>
2020-08-07 16:04:34 +0800

    对负例子生成进行了改进，但样本比例不对，需手动修改

--- NNTools/ImageCrop.py
+++ NNTools/ImageCrop.py
@@ -11,9 +11,9 @@ import random
 
 #配置信息
 # anno_file=r"C:\Users\Administrator\Desktop\gened_data\label"
-img_file=r"C:\Users\Administrator\Desktop\gened_data\img_celeba"
-save_path=r"C:\Users\Administrator\Desktop\gened_data"
-label_txt=r'C:\Users\Administrator\Desktop\gened_data\list_bbox_celeba.txt'
+img_file=r"C:\Users\Administrator\Desktop\test500\img500"
+save_path=r"C:\Users\Administrator\Desktop\test500"
+label_txt=r'C:\Users\Administrator\Desktop\test500\500_label.txt'
 # anno_file=r"C:\Users\李梓桦\Desktop\test_data\label"
 # img_file=r"C:\Users\李梓桦\Desktop\test_data\img"
 # save_path=r"C:\Users\李梓桦\Desktop\test_data"
@@ -91,7 +91,7 @@ for size in face_size:
                     cy=topy+h/2#cy:center y
                     # 生成部分人脸和人脸的图片
 
-                    for _ in range(6):
+                    for _ in range(3):
                          #直接使用np.random.randint似乎有bug
 
                         w_=np.random.randint(-w*0.25,w*0.26)#横向随机偏移0.2w
@@ -147,27 +147,32 @@ for size in face_size:
                                 pass
                         #time.sleep(0.002)
                     #负样本生成
-                    for i in range(6):#数量一般和上面一样
+                    for i in range(3):#数量一般和上面一样
 
                         offset_len=np.random.randint(size,min(img_w,img_h)/2)# 偏移量
                         x1_=np.random.randint(0,img_w-offset_len)#生成左上角的坐标,生成的patch都在左上角
                         y1_=np.random.randint(0,img_h-offset_len)
                         x1_=max(0,x1_)
                         y1_=max(0,y1_)
-                        crop_box=np.array([x1_,y1_,x1_+offset_len,y1_+offset_len])
+                        crop_box1=np.array([x1_,y1_,x1_+offset_len,y1_+offset_len])
 
-                        # x2_=np.random.randint(min(img_w,bottonx+offset_len),max(img_w,bottonx+offset_len))#生成左上角的坐标，使得左上角坐标落在图片下部分
-                        # y2_=np.random.randint(min(img_h,bottony+offset_len),max(img_h,bottony+offset_len))
-                        # offset=np.random.randint(0.75*size,size)
-                        # xx2_=min(img_w,bottonx+offset)
-                        # yy2_=min(img_h,bottony+offset_len)
-                        # crop_box2=[x2_,y2_,xx2_,yy2_]
-                        #
-                        # #有越界
-                        # crop_boxes=[crop_box1,crop_box2]
-                        # crop_box=np.random.choice(crop_boxes)
+                        offset_top=np.random.randint(0.75*size,1.25*size)
+                        x2_=np.random.randint(bottonx,bottonx+offset_top)#生成左上角的坐标，使得左上角坐标落在图片下部分
+                        y2_=np.random.randint(bottony,bottony+offset_top)
+
+                        xx2_=min(img_w-1,bottonx+offset_len)
+                        yy2_=min(img_h-1,bottony+offset_len)
+                        crop_box2=[x2_,y2_,xx2_,yy2_]
+
+
+                        crop_boxes=[crop_box1,crop_box2]
+                        idx=np.random.randint(0,2)
+                        crop_box=crop_boxes[idx]
+
                         # rect=[1.0,crop_box[0],crop_box[1],crop_box[2],crop_box[3]]
                         # draw_rect(img_path,[rect])
+
+
 
                         if iou(crop_box,np.array(boxes))<0.29:
                             face_crop = img.crop(crop_box)  # 抠图
@@ -186,6 +191,3 @@ for size in face_size:
         pos_anno_file.close()
         neg_anno_file.close()
         part_anno_file.close()
-
-
-
